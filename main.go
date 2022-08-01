d026027686b39566be3ed832e0a3bbae859fc8eb
hawken <askhaokun@gmail.com>
2022-08-01 17:18:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/fox-one/mixin-sdk-go"
-	"github.com/gofrs/uuid"
 )
 
 var (
 	// Specify the keystore file in the -config parameter
 	config = flag.String("config", "", "keystore file path")
+	text   = flag.String("text", "hello world", "text message")
 )
 
 func main() {
@@ -41,56 +39,43 @@ func main() {
 		log.Panicln(err)
 	}
 
-	// Prepare the message loop that handle every incoming messages,
-	// and reply it with the same content.
-	// We use a callback function to handle them.
-	h := func(ctx context.Context, msg *mixin.MessageView, userID string) error {
-		// if there is no valid user id in the message, drop it
-		if userID, _ := uuid.FromString(msg.UserID); userID == uuid.Nil {
-			return nil
-		}
+	ctx := context.Background()
 
-		// The incoming message's message ID, which is an UUID.
-		id, _ := uuid.FromString(msg.MessageID)
-
-		// Create a request
-		reply := &mixin.MessageRequest{
-			// Reuse the conversation between the sender and the bot.
-			// There is an unique UUID for each conversation.
-			ConversationID: msg.ConversationID,
-			// The user ID of the recipient.
-			// The bot will reply messages, so here is the sender's ID of each incoming message.
-			RecipientID: msg.UserID,
-			// Create a new message id to reply, it should be an UUID never used by any other message.
-			// Create it with a "reply" and the incoming message ID.
-			MessageID: uuid.NewV5(id, "reply").String(),
-			// The bot just reply the same category and the same content of the incoming message
-			// So, we copy the category and data
-			Category: msg.Category,
-			//Data:     msg.Data,
-			Data: "hello world",
-		}
-		// Send the response
-		return client.SendMessage(ctx, reply)
+	me, err := client.UserMe(ctx)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	ctx, stop := signal.NotifyContext(
-		context.Background(),
-		syscall.SIGINT,
-		syscall.SIGTERM,
-	)
-	defer stop()
-
-	// Start the message loop.
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(time.Second):
-			// Pass the callback function into the `BlazeListenFunc`
-			if err := client.LoopBlaze(ctx, mixin.BlazeListenFunc(h)); err != nil {
-				log.Printf("LoopBlaze: %v", err)
-			}
-		}
+	if me.App == nil {
+		log.Fatalln("use a bot keystore instead")
 	}
+
+	receiptID := me.App.CreatorID
+
+	sessions, err := client.FetchSessions(ctx, []string{receiptID})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_ = sessions
+
+	req := &mixin.MessageRequest{
+		ConversationID: mixin.UniqueConversationID(client.ClientID, receiptID),
+		RecipientID:    receiptID,
+		MessageID:      mixin.RandomTraceID(),
+		Category:       mixin.MessageCategoryPlainText,
+		Data:           base64.StdEncoding.EncodeToString([]byte(*text)),
+	}
+
+	if err := client.EncryptMessageRequest(req, sessions); err != nil {
+		log.Fatalln(err)
+	}
+
+	receipts, err := client.SendEncryptedMessages(ctx, []*mixin.MessageRequest{req})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	b, _ := json.Marshal(receipts)
+	log.Println(string(b))
 }
